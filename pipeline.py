901cb3e6dc26409631b4c049d4051499073b8242
jvmaluballer <jvmaluballer@gmail.com>
2024-05-29 01:50:42 -0700

    Updated my section
    Updated initial file input and selection section. Updated conversion and parsing of the r and i instructions. Issues with the immediate instructions so far.

--- pipeline.py
+++ pipeline.py
@@ -1,127 +1,206 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  6 19:53:11 2024
-
-@author: 
-    Dmitrii Fotin       <dfotin@pdx.edu>
-    John Michael Mertz  <jmertz@pdx.edu>  
-    Josh Varughese       <jv23@pdx.edu>
-"""
-
 while True:
-    
-    #file input
+    # file input
     file_input = input("Enter file name: ")
-    #ask if forwarding or non-forwarding
-    print_message = input("Select mode:\n1 - Functional Simulator only\n2 - Functional Simulator + non-forwarding Timing Simulator\n3 - Functional Simulator + forwarding Timing Simulator")
-    while print_message != '1' and print_message != '2' and print_message != '3':
-        print_message = input("Invalid Input! Select mode:\n1 - Functional Simulator only\n2 - Functional Simulator + non-forwarding Timing Simulator\n3 - Functional Simulator + forwarding Timing Simulator") 
+    # ask if forwarding or non-forwarding
+    print_message = input("Select mode:\n1 - Functional Simulator only\n2 - Functional Simulator + non-forwarding Timing Simulator\n3 - Functional Simulator + forwarding Timing Simulator\n")
+    while print_message not in {'1', '2', '3'}:
+        print_message = input("Invalid Input! Select mode:\n1 - Functional Simulator only\n2 - Functional Simulator + non-forwarding Timing Simulator\n3 - Functional Simulator + forwarding Timing Simulator\n")
     
-    #create a pipeline array
+    # create a pipeline array
     pipeline = []
     stalls = 0
     cycles = 0
-    
-    
-    #open the specified file
-    with open(file_input, 'r') as file:
-        while True:
-            #read file line by line
-            data = file.readline()
-            #if file is empty, exit and ask for next file input
-            if not data:
-                break
-            #if last line in file reached, ask whether to exit the program
-            #or enter next file name
-            if data == "\n":
-                print("Empty Line reached.")
-                print("Continue? y/n")
-                if input() == 'n':
+
+    try:
+        # open the specified file
+        with open(file_input, 'r') as file:
+            while True:
+                # read file line by line
+                data = file.readline().strip()
+                # if file is empty, exit and ask for next file input
+                if not data:
                     break
-                mode = None
-            #for commands that are not 8 or 9, unpack line into command and hex address
-            else:
+                
+                # convert command to binary
                 address = data.zfill(8)
-                #convert address to binary
-                pipeline[0] = ''.join(bin(int(i,16))[2:].zfill(4) for i in address)
-            
-            #ADD
-            if pipeline[0][0:5] == 0b000000:
-                print(pipeline[0][0:5])
+                binary_command = ''.join(bin(int(char, 16))[2:].zfill(4) for char in address)
+                pipeline.insert(0, binary_command)
                 
-            #ADDI
-            elif pipeline[0][0:5] == 0b000001:
-                print(pipeline[0][0:5])
+                # Check commands
+                opcode = int(pipeline[0][:6], 2)
                 
-            #SUB
-            elif pipeline[0][0:5] == 0b000010:
-                print(pipeline[0][0:5])
+#               -------------Josh's Portion-------------
                 
-            #SUBI
-            elif pipeline[0][0:5] == 0b000011:
-                print(pipeline[0][0:5])
+                # ADD
+                if opcode == 0b000000:
+                    print("\nADD command found:", pipeline[0][:6])
+                    rs = pipeline[0][6:11]
+                    print("Source Register Contents (rs): ", rs)
+                    rt = pipeline[0][11:16]
+                    print("Source Register Contents (rt): ", rt)
+                    rd = pipeline[0][16:21]
+                    print("Destination Register Contents: ", rd)
+                    
+                    # convert to decimal to perform addition
+                    rs_int = int(rs, 2)
+                    rt_int = int(rt, 2)
+                    rd_int = rs_int + rt_int
+                    
+                    # convert back to binary to display
+                    rd = bin(rd_int)[2:].zfill(5)
+                    print("Updated Destination Register Contents: ", rd)
+                    
+                # ADDI
+                elif opcode == 0b000001:
+                    print("\nADDI command found:", pipeline[0][:6])
+                    rs = pipeline[0][6:11]
+                    print("Source Register Contents (rs): ", rs)
+                    rt = pipeline[0][11:16]
+                    print("Destination Register Contents: ", rt)
+                    imm = pipeline[0][16:]
+                    print("Immediate Register Contents (imm): ", imm)
+                    
+                    # convert to decimal to perform addition
+                    rs_int = int(rs, 2)
+                    imm_int = int(imm, 2)
+                    rt_int = rs_int + imm_int
+                    
+                    # convert back to binary to display
+                    rt = bin(rt_int)[2:].zfill(5)
+                    print("Updated Destination Register Contents: ", rt)
+                    
+                # SUB
+                elif opcode == 0b000010:
+                    print("\nSUB command found:", pipeline[0][:6])
+                    rs = pipeline[0][6:11]
+                    print("Source Register Contents (rs): ", rs)
+                    rt = pipeline[0][11:16]
+                    print("Source Register Contents (rt): ", rt)
+                    rd = pipeline[0][16:21]
+                    print("Destination Register Contents: ", rd)
+                    
+                    # convert to decimal to perform subtraction
+                    rs_int = int(rs, 2)
+                    rt_int = int(rt, 2)
+                    rd_int = rs_int - rt_int
+                    
+                    # convert back to binary to display
+                    rd = bin(rd_int)[2:].zfill(5)
+                    print("Updated Destination Register Contents: ", rd)
+                    
+                # SUBI
+                elif opcode == 0b000011:
+                    print("\nSUBI command found:", pipeline[0][:6])
+                    rs = pipeline[0][6:11]
+                    print("Source Register Contents (rs): ", rs)
+                    rt = pipeline[0][11:16]
+                    print("Destination Register Contents: ", rt)
+                    imm = pipeline[0][16:]
+                    print("Immediate Register Contents (imm): ", imm)
+                    
+                    # convert to decimal to perform addition
+                    rs_int = int(rs, 2)
+                    imm_int = int(imm, 2)
+                    rt_int = rs_int - imm_int
+                    
+                    # convert back to binary to display
+                    rt = bin(rt_int)[2:].zfill(5)
+                    print("Updated Destination Register Contents: ", rt)
+                    
+                # MUL
+                elif opcode == 0b000100:
+                    print("\nMUL command found:", pipeline[0][:6])
+                    rs = pipeline[0][6:11]
+                    print("Source Register Contents (rs): ", rs)
+                    rt = pipeline[0][11:16]
+                    print("Source Register Contents (rt): ", rt)
+                    rd = pipeline[0][16:21]
+                    print("Destination Register Contents: ", rd)
+                    
+                    # convert to decimal to perform subtraction
+                    rs_int = int(rs, 2)
+                    rt_int = int(rt, 2)
+                    rd_int = rs_int * rt_int
+                    
+                    # convert back to binary to display
+                    rd = bin(rd_int)[2:].zfill(5)
+                    print("Updated Destination Register Contents: ", rd)
+                    
+                # MULI
+                elif opcode == 0b000101:
+                    print("\nMULI command found:", pipeline[0][:6])
+                    rs = pipeline[0][6:11]
+                    print("Source Register Contents (rs): ", rs)
+                    rt = pipeline[0][11:16]
+                    print("Destination Register Contents: ", rt)
+                    imm = pipeline[0][16:]
+                    print("Immediate Register Contents (imm): ", imm)
+                    
+                    # convert to decimal to perform addition
+                    rs_int = int(rs, 2)
+                    imm_int = int(imm, 2)
+                    rt_int = rs_int * imm_int
+                    
+                    # convert back to binary to display
+                    rt = bin(rt_int)[2:].zfill(5)
+                    print("Updated Destination Register Contents: ", rt)
+                    
+#               ------------End Josh's Portion-------------
+              
+                # OR
+                elif opcode == 0b000110:
+                    print("OR command found:", pipeline[0][:6])
+                    
+                # ORI
+                elif opcode == 0b000111:
+                    print("ORI command found:", pipeline[0][:6])
+                    
+                # AND
+                elif opcode == 0b001000:
+                    print("AND command found:", pipeline[0][:6])
+                    
+                # ANDI
+                elif opcode == 0b001001:
+                    print("ANDI command found:", pipeline[0][:6])
+                    
+                # XOR
+                elif opcode == 0b001010:
+                    print("XOR command found:", pipeline[0][:6])
+                    
+                # XORI
+                elif opcode == 0b001011:
+                    print("XORI command found:", pipeline[0][:6])
+                    
+                # LDW
+                elif opcode == 0b001100:
+                    print("LDW command found:", pipeline[0][:6])
+                    
+                # STW
+                elif opcode == 0b001101:
+                    print("STW command found:", pipeline[0][:6])
+                    
+                # BZ
+                elif opcode == 0b001110:
+                    print("BZ command found:", pipeline[0][:6])
+                    
+                # BEQ
+                elif opcode == 0b001111:
+                    print("BEQ command found:", pipeline[0][:6])
+                    
+                # JR
+                elif opcode == 0b010000:
+                    print("JR command found:", pipeline[0][:6])
+                    
+                # HALT
+                elif opcode == 0b010001:
+                    print("HALT command found:", pipeline[0][:6])
+                    
+                else:
+                    print("Invalid opcode entered for given command line: " + data)
+                    continue
                 
-            #MUL
-            elif pipeline[0][0:5] == 0b000100:
-                print(pipeline[0][0:5])
-                
-            #MULI
-            elif pipeline[0][0:5] == 0b000101:
-                print(pipeline[0][0:5])
-                
-            #OR
-            elif pipeline[0][0:5] == 0b000110:
-                print(pipeline[0][0:5])
-                
-            #ORI
-            elif pipeline[0][0:5] == 0b000111:
-                print(pipeline[0][0:5])
-                
-            #AND
-            elif pipeline[0][0:5] == 0b001000:
-                print(pipeline[0][0:5])
-                
-            #ANDI
-            elif pipeline[0][0:5] == 0b001001:
-                print(pipeline[0][0:5])
-                
-            #XOR
-            elif pipeline[0][0:5] == 0b001010:
-                print(pipeline[0][0:5])
-                
-            #XORI
-            elif pipeline[0][0:5] == 0b001011:
-                print(pipeline[0][0:5])
-                
-            #LDW
-            elif pipeline[0][0:5] == 0b001100:
-                print(pipeline[0][0:5])
-                
-            #STW
-            elif pipeline[0][0:5] == 0b001101:
-                print(pipeline[0][0:5])
-                
-            #BZ
-            elif pipeline[0][0:5] == 0b001110:
-                print(pipeline[0][0:5])
-                
-            #BEQ
-            elif pipeline[0][0:5] == 0b001111:
-                print(pipeline[0][0:5])
-                
-            #JR
-            elif pipeline[0][0:5] == 0b010000:
-                print(pipeline[0][0:5])
-                
-            #HALT
-            elif pipeline[0][0:5] == 0b010001:
-                print(pipeline[0][0:5])
-                
-            else:
-                print("Invalid opcode entered for given command line: " + data)
-                continue
-                
-        print("End of file reached")
-        if input("Exit program? y/n\n") == 'y':
-            break
-        
+            print("End of file reached")
+            if input("Exit program? y/n\n") == 'y':
+                break
+    except FileNotFoundError:
+        print("File not found. Please try again.")
